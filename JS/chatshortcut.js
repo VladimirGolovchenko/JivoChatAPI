6e7a9e1a3704ce8356f6a8dbee5efa1c180ac341
Vladimir Golovchenko <vladivego@gmail.com>
2021-08-29 00:18:08 +0300

    commit

---
+++ JS/chatshortcut.js
@@ -0,0 +1,37 @@
+<div id="gsl_contacts_container" class="gsl-contacts-container">
+            <a href="#" class="contacts-link link-jivosite" data-tooltip="Чат с консультантом">
+            <span class="contacts-badge">
+                <i class="contacts-icon icon-jivosite"></i>
+            </span>
+        </a>
+        <a href="https://gsl.org/offshore-lawyer-online/cp/?conferenceId=c6cf3c4c-ce97-11eb-9f6f-525400d46e50" class="contacts-link link-videoconference" data-tooltip="Видеоконсультация" target="_blank">
+        <span class="contacts-badge">
+            <span class="contacts-icon icon-videoconference"></span>
+        </span>
+    </a>
+    <a href="#" class="contacts-link link-redconnect contacts-link_disabled" data-tooltip="Заказать звонок возможно с 9.00 до 21.00 UTC+3">
+        <span class="contacts-badge">
+            <i class="contacts-icon icon-redconnect"></i>
+        </span>
+    </a>
+    <a href="https://wa.me/74993725115" class="contacts-link link-whatsapp" target="_blank" data-tooltip="Whatsapp">
+        <span class="contacts-badge">
+            <i class="contacts-icon icon-whatsapp"></i>
+        </span>
+    </a>
+    <a href="https://t.me/get_offshore_bot" class="contacts-link link-telegram" target="_blank" data-tooltip="Telegram">
+        <span class="contacts-badge">
+            <i class="contacts-icon icon-telegram"></i>
+        </span>
+    </a>
+    <a href="viber://pa?chatURI=offshoremixer" class="contacts-link link-viber" data-tooltip="Viber">
+        <span class="contacts-badge">
+            <i class="contacts-icon icon-viber"></i>
+        </span>
+    </a>
+    <a href="mailto:gsl@gsl.org" class="contacts-link link-mail" data-tooltip="E-mail">
+        <span class="contacts-badge">
+            <i class="contacts-icon icon-mail"></i>
+        </span>
+    </a>
+</div>
