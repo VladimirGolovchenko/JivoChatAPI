e9ef776cdeb8551ed7b3af36003300f1df4a8dc4
Vladimir <vladivego@gmail.com>
2021-11-10 10:52:49 +0300

    Mes

---
+++ JS/roundedbutton.js
@@ -0,0 +1,133 @@
+
+function jivo_onLoadCallback() {
+    function jivo_onLoadCallback() {
+        if (!/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/i.test(navigator.userAgent)) {
+            //jivo_custom_widget:
+        }
+    }
+}
+
+
+<jdiv class="globalClass_2cc9">
+    <jdiv
+        class="wrap_b362 _orientationRight_75db __jivoMobileButton"
+        style="zoom: 2.3913;"
+    >
+        <jdiv
+            class="button_5724"
+            style="background: linear-gradient(95deg, rgb(255, 106, 0) 20%, rgb(238, 9, 121) 80%);"
+        >
+            <jdiv class="messagesLabel_a005">
+                <jdiv class="badge_33cd">1</jdiv>
+            </jdiv>
+            <jdiv class="iconWrap_d567 _isCallEnabled_91e1 _showLogo_f240">
+                <jdiv
+                    class="jivoIcon_1be9 icons_2a4c"
+                    style='background-image: url("data:image/svg+xml,%3Csvg%20width%3D%2210%22%20height%3D%2235%22%20viewBox%3D%220%200%209%2035%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20fill%3D%22%23fff%22%20d%3D%22M1.31%2034.966H.975c-.295%200-.53-.09-.71-.27C.09%2034.516%200%2034.3%200%2034.05v-2.33c0-.25.097-.465.288-.645.193-.18.42-.27.687-.27H1.3c1.776%200%203.223-1.37%203.238-3.06v-8.94H.975c-.236%200-.458-.09-.665-.27-.206-.18-.31-.395-.31-.645v-2.33c0-.223.104-.43.31-.625.207-.194.43-.292.665-.292h7.019c.266%200%20.494.09.687.27.192.182.29.396.29.646V27.77c0%20.032-.008.058-.01.09-.08%203.937-3.477%207.098-7.65%207.104zM.318%200C-.25%2011.117%208.62%2012.702%208.62%2012.702%208.17.522.318%200%20.318%200z%22%2F%3E%3C%2Fsvg%3E%0A");'
+                ></jdiv>
+            </jdiv>
+            <jdiv class="iconWrap_d567 _isCallEnabled_91e1 _showLogo_f240 iconWrap2_fc72">
+                <jdiv
+                    class="envelopeIcon_c9c4 icons_2a4c"
+                    style="background-image: url(&quot;data:image/svg+xml,%3Csvg%20width%3D%2225%22%20height%3D%2220%22%20viewBox%3D%220%200%2025%2020%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cpath%20fill%3D'%23fff'%20d%3D%22M22.5%205l-10%206.25L2.5%205V2.5l10%206.25%2010-6.25V5zm0-5h-20A2.49%202.49%200%200%200%200%202.5v15A2.5%202.5%200%200%200%202.5%2020h20a2.5%202.5%200%200%200%202.5-2.5v-15A2.5%202.5%200%200%200%2022.5%200z%22%2F%3E%3C%2Fsvg%3E%0A&quot;);"
+                ></jdiv>
+            </jdiv>
+            <jdiv class="iconWrap_d567 _isCallEnabled_91e1 _showLogo_f240 iconWrap3_c10a">
+                <jdiv
+                    class="callbackIcon_1f6d icons_2a4c"
+                    style='background-image: url("data:image/svg+xml,%3Csvg%20width%3D%2233%22%20height%3D%2233%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%3E%3Cdefs%3E%3Cpath%20fill%3D%22%23fff%22%20d%3D%22M24.2%2033.433c2.411%204.823%206.544%208.784%2011.367%2011.367l3.789-3.789c.516-.517%201.205-.689%201.722-.344%201.894.689%203.96%201.033%206.2%201.033%201.033%200%201.722.689%201.722%201.722v5.856C49%2050.31%2048.311%2051%2047.278%2051%2031.088%2051%2018%2037.911%2018%2021.722%2018%2020.69%2018.689%2020%2019.722%2020h6.028c1.033%200%201.722.689%201.722%201.722%200%202.067.345%204.134%201.034%206.2.172.517%200%201.206-.345%201.722l-3.961%203.79z%22%20id%3D%22a%22%2F%3E%3C%2Fdefs%3E%3Cuse%20fill%3D%22%23FFF%22%20xlink%3Ahref%3D%22%23a%22%20transform%3D%22translate(-17%20-20)%22%20fill-rule%3D%22evenodd%22%2F%3E%3C%2Fsvg%3E%0A");'
+                ></jdiv>
+            </jdiv>
+        </jdiv>
+    </jdiv>
+    <jdiv class="wrap_5b1e" style="display: none;">
+        <jdiv class="zoom_a1fb"></jdiv>
+        <jdiv class="substrate_27dc"></jdiv>
+        <jdiv
+            class="iconContainer_ae59"
+            style="width: 100%; height: 1761px; display: block;"
+        >
+            <jdiv
+                class="viewWrap_cfb2"
+                style="width: 990px; height: 1760px; transform: translate(0px, 0px);"
+            >
+                <jdiv class="box_bd4f" style="zoom: 2.3913;">
+                    <jdiv>
+                        <jdiv iconname="hand" class="handIcon_0683">
+                            <svg
+                                width="160"
+                                height="173"
+                                viewBox="0 0 160 173"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <title>hand</title>
+                                <path
+                                    d="M56.31 169.5S8.685 109 5.968 106.25c-2.717-2.75-5.84-12.663 0-20.05C11.807 78.816 21.295 82.95 24 85c2.705 2.05 13.06 10.5 13.06 10.5s7.646 6.35 14.5 2.093c6.857-4.256 6.42-11.392 6.42-11.392s1.93-16.115-1.67-35.2C52.714 31.915 49 25.25 46.75 20c-2.25-5.25-1.124-11.59 5.938-16.045C59.75-.5 67 6.5 68.5 9.25s6.396 9.49 12.12 27.253c5.722 17.763 7.69 41.372 7.69 41.372l-2-23.25c5.576 0 13.464-2.994 24.017 9.168 4.736 5.457 6.86 24.332 6.86 24.332s-1.874-15.188-5.25-21.75c4.5-1.25 12.583-4.006 21.376 4.875 6.31 6.375 5.375 23.25 5.375 23.25s1.25-13.063-4.25-21.938c13.68-1.527 19.937 11.313 21.937 19.563 2 8.25-.875 26.75-.875 26.75l-14.19 51.875"
+                                    stroke="#A3AFB8"
+                                    fill-rule="nonzero"
+                                    stroke-width="4"
+                                    fill="#FFF"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                ></path>
+                            </svg>
+                        </jdiv>
+                        <jdiv iconname="arrow" class="arrow1_5560">
+                            <svg
+                                width="59"
+                                height="71"
+                                viewBox="0 0 59 71"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <title>Arrow1</title>
+                                <g
+                                    transform="rotate(163 27.277 33.133)"
+                                    fill="#00A355"
+                                    fill-rule="evenodd"
+                                >
+                                    <rect y="12" width="8" height="48" rx="4"></rect>
+                                    <rect y="52" width="48" height="8" rx="4"></rect>
+                                    <rect
+                                        transform="rotate(-45 30.523 29.475)"
+                                        x="-6.477"
+                                        y="25.475"
+                                        width="74"
+                                        height="8"
+                                        rx="4"
+                                    ></rect>
+                                </g>
+                            </svg>
+                        </jdiv>
+                        <jdiv iconname="arrow2" class="arrow2_bcf4">
+                            <svg
+                                width="59"
+                                height="71"
+                                viewBox="0 0 59 71"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <title>Arrow2</title>
+                                <g
+                                    transform="rotate(-17 45.338 20.625)"
+                                    fill="#00A355"
+                                    fill-rule="evenodd"
+                                >
+                                    <rect y="12" width="8" height="48" rx="4"></rect>
+                                    <rect y="52" width="48" height="8" rx="4"></rect>
+                                    <rect
+                                        transform="rotate(-45 30.523 29.475)"
+                                        x="-6.477"
+                                        y="25.475"
+                                        width="74"
+                                        height="8"
+                                        rx="4"
+                                    ></rect>
+                                </g>
+                            </svg>
+                        </jdiv>
+                    </jdiv>
+                </jdiv>
+            </jdiv>
+        </jdiv>
+    </jdiv>
+    <jdiv class="pseudoHeight_da6e"></jdiv>
+</jdiv>;
